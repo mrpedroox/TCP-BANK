094ae2206881c30c01143241d62e626bd4952fbf
mrpedroox <pedrohenriquetavares06@gmail.com>
2026-05-25 19:08:03 -0300

    treating exceptions in generic bank

--- src/banks/GenericBank.java
+++ src/banks/GenericBank.java
@@ -4,6 +4,8 @@ import accounts.AbstractAccount;
 import accounts.EspecialAccount;
 import accounts.IAccountRepo;
 import accounts.SavingsAccount;
+import exceptions.AIException;
+import exceptions.BIException;
 
 public class GenericBank implements IBank{
     private IAccountRepo accounts;
@@ -39,48 +41,51 @@ public class GenericBank implements IBank{
     }
 
     @Override
-    public void credit(String id, double value){
+    public void credit(String id, double value) throws AIException{
         AbstractAccount a = accounts.search(id);
-        if(a != null){
-            a.credit(value);
-        }
+        if(a == null) throw new AIException(id);
+        a.credit(value);
     }
 
     @Override
-    public void debit(String id, double value){
+    public void debit(String id, double value) throws AIException, BIException {
         AbstractAccount a = accounts.search(id);
-        if(a!=null){
-            a.debit(value);
-        }
+        if(a == null) throw new AIException(id);
+        if(a.getBalance() < value) throw new BIException(id, a.getBalance());
+        a.debit(value);
     }
 
     @Override
-    public double balance(String id){
+    public double balance(String id) throws AIException{
         AbstractAccount a = accounts.search(id);
+        if(a==null) throw new AIException(id);
         return a.getBalance();
     }
 
     @Override
-    public void transfer(String origin, String destiny, double value){
+    public void transfer(String origin, String destiny, double value) throws AIException, BIException{
         AbstractAccount org = accounts.search(origin);
+        if(org == null) throw new AIException(origin);
         AbstractAccount des = accounts.search(destiny);
-        if(org!=null && des!=null){
-            org.debit(value);
-            des.credit(value);
-        }
+        if(des == null) throw new AIException(destiny);
+        if(org.getBalance()<value) throw new BIException(origin, org.getBalance());
+        org.debit(value);
+        des.credit(value);
     }
 
     @Override
-    public void runBonus(String id){
+    public void runBonus(String id) throws AIException{
         AbstractAccount a = accounts.search(id);
+        if(a == null) throw new AIException(id);
         if(a instanceof EspecialAccount){
             ((EspecialAccount) a).runBonus();
         }
     }
 
     @Override
-    public void runFees(String id){
+    public void runFees(String id) throws AIException{
         AbstractAccount a = accounts.search(id);
+        if(a==null) throw new AIException(id);
         if(a instanceof SavingsAccount){
             ((SavingsAccount) a).fees(fee);
         }
--- src/banks/IBank.java
+++ src/banks/IBank.java
@@ -1,16 +1,18 @@
 package banks;
 
 import accounts.AbstractAccount;
+import exceptions.AIException;
+import exceptions.BIException;
 
 public interface IBank {
     public double totalBalance();
     public int totalAccounts();
     public void register(AbstractAccount account);
     public AbstractAccount find(String id);
-    public void credit(String id, double value);
-    public void debit(String id, double value);
-    public double balance(String id);
-    public void transfer(String origin, String destiny, double value);
-    public void runBonus(String id);
-    public void runFees(String id);
+    public void credit(String id, double value) throws AIException;
+    public void debit(String id, double value) throws AIException, BIException;
+    public double balance(String id) throws AIException;
+    public void transfer(String origin, String destiny, double value) throws AIException, BIException;
+    public void runBonus(String id) throws AIException;
+    public void runFees(String id) throws AIException;
 }
