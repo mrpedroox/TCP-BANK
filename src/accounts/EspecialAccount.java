41a7dc09825e72bc13de3a34b97105b7f234505a
mrpedroox <pedrohenriquetavares06@gmail.com>
2026-05-25 16:58:45 -0300

    first implementation of polymorphism

--- src/accounts/EspecialAccount.java
+++ src/accounts/EspecialAccount.java
@@ -1,5 +1,19 @@
 package accounts;
 
 public class EspecialAccount extends Account{
+    private double bonus;
+    public EspecialAccount(String id){
+        super(id);
+    }
 
+    public void runBonus(){
+        super.credit(bonus);
+        bonus = 0;
+    }
+
+    @Override
+    public void credit(double value){
+        bonus += value*0.01;
+        super.credit(value);
+    }
 }
