c4f0cb3ebb9024a22ba91aa18d479b9332487377
mrpedroox <pedrohenriquetavares06@gmail.com>
2026-05-25 13:54:50 -0300

    finshing implementing abstract class

--- src/accounts/AbstractAccount.java
+++ src/accounts/AbstractAccount.java
@@ -1,5 +1,25 @@
 package accounts;
 
-public class AbstractAccount {
+public abstract class AbstractAccount {
+    protected String id;
+    protected double balance;
 
+    public AbstractAccount(String id){
+        this.id = id;
+        balance = 0;
+    }
+
+    public void credit(double value){
+        balance += value;
+    }
+
+    public abstract void debit(double value);
+
+    public String getId(){
+        return id;
+    }
+
+    public double getBalance(){
+        return balance;
+    }
 }
