72fd58cc951a14d06c1b47be863aa03cbe06cc2a
mrpedroox <pedrohenriquetavares06@gmail.com>
2026-05-25 18:17:06 -0300

    finally finished training interfaces with the GenericBank class

--- src/banks/GenericBank.java
+++ src/banks/GenericBank.java
@@ -1,10 +1,13 @@
 package banks;
 
 import accounts.AbstractAccount;
+import accounts.EspecialAccount;
 import accounts.IAccountRepo;
+import accounts.SavingsAccount;
 
 public class GenericBank implements IBank{
     private IAccountRepo accounts;
+    private double fee;
 
     public GenericBank(IAccountRepo accounts){
         this.accounts = accounts;
@@ -12,6 +15,75 @@ public class GenericBank implements IBank{
 
     @Override
     public void register(AbstractAccount account) {
-
+        accounts.insert(account);
     }
+
+    @Override
+    public AbstractAccount find(String id){
+        return accounts.search(id);
+    }
+
+    @Override
+    public int totalAccounts(){
+        return accounts.sizee();
+    }
+
+    @Override
+    public double totalBalance(){
+        AbstractAccount[] array = accounts.listt();
+        double t = 0;
+        for(int i = 0; i < accounts.sizee(); i++){
+            t += array[i].getBalance();
+        }
+        return t;
+    }
+
+    @Override
+    public void credit(String id, double value){
+        AbstractAccount a = accounts.search(id);
+        if(a != null){
+            a.credit(value);
+        }
+    }
+
+    @Override
+    public void debit(String id, double value){
+        AbstractAccount a = accounts.search(id);
+        if(a!=null){
+            a.debit(value);
+        }
+    }
+
+    @Override
+    public double balance(String id){
+        AbstractAccount a = accounts.search(id);
+        return a.getBalance();
+    }
+
+    @Override
+    public void transfer(String origin, String destiny, double value){
+        AbstractAccount org = accounts.search(origin);
+        AbstractAccount des = accounts.search(destiny);
+        if(org!=null && des!=null){
+            org.debit(value);
+            des.credit(value);
+        }
+    }
+
+    @Override
+    public void runBonus(String id){
+        AbstractAccount a = accounts.search(id);
+        if(a instanceof EspecialAccount){
+            ((EspecialAccount) a).runBonus();
+        }
+    }
+
+    @Override
+    public void runFees(String id){
+        AbstractAccount a = accounts.search(id);
+        if(a instanceof SavingsAccount){
+            ((SavingsAccount) a).fees(fee);
+        }
+    }
+
 }
